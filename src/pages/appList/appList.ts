d0f9b1855f9c54d1a96e4cf63fdd7fa12bfdb858
Robin Carozzani <contact@robincarozzani.fr>
2018-04-15 17:25:43 +0200

    applications list template

--- src/pages/appList/appList.ts
+++ src/pages/appList/appList.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { Application } from '../../model/application';
 
 @Component({
   selector: 'page-appList',
   templateUrl: 'appList.html'
 })
-export class AppList {}
+export class AppList {
+  public applications: Application[];
+
+  constructor() {}
+
+  ionViewDidLoad() {
+    this.applications = [];
+  }
+}
