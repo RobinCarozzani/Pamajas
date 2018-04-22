e1b64f8a2c0483efbaee5ef4338bcdfaeb484e93
Robin Carozzani <contact@robincarozzani.fr>
2018-04-22 10:08:22 +0200

    application details controller

--- src/pages/appDetail/appDetail.ts
+++ src/pages/appDetail/appDetail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Application } from '../../model/application';
-import { NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { CryptoService } from '../../services/crypto.service';
+import { NewApp } from '../newApp/newApp';
 
 @Component({
   selector: 'page-appDetail',
@@ -8,10 +10,31 @@ import { NavParams } from 'ionic-angular';
 })
 export class AppDetail {
   public currentApplication: Application;
+  public displayedPassword: string = '*****';
+  public passwordIsVisible: boolean = false;
 
-  constructor(private navParams: NavParams) {}
+  constructor(
+    private cryptoService: CryptoService,
+    private navCtrl: NavController,
+    private navParams: NavParams
+  ) {}
 
   ionViewDidLoad() {
     this.currentApplication = this.navParams.get('application');
+  }
+
+  public tooglePasswordVisibility(): void {
+    if (!this.passwordIsVisible) {
+      this.displayedPassword = this.cryptoService.decypher(
+        this.currentApplication.credentials
+      );
+    } else {
+      this.displayedPassword = '*****';
+    }
+    this.passwordIsVisible = !this.passwordIsVisible;
+  }
+
+  public edit(): void {
+    this.navCtrl.push(NewApp, { application: this.currentApplication });
   }
 }
