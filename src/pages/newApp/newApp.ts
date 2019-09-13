177ace527d1199cc17798f1efe7760d6c8786fa8
Robin Carozzani <contact@robincarozzani.fr>
2019-09-13 21:32:15 +0200

    new/edit app page

--- src/pages/newApp/newApp.ts
+++ src/pages/newApp/newApp.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import { Application } from '../../model/application';
+import { NavParams } from 'ionic-angular';
+import { Credentials } from '../../model/credentials';
 
 @Component({
   selector: 'page-newApp',
   templateUrl: 'newApp.html'
 })
-export class NewApp {}
+export class NewApp {
+  public application: Application;
+  public editionMode: boolean;
+
+  constructor(private navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    const appFromNav: Application = this.navParams.get('application');
+    if (appFromNav != null) {
+      this.application = appFromNav;
+      this.editionMode = true;
+    } else {
+      this.application = new Application();
+      this.application.credentials = new Credentials();
+      this.editionMode = false;
+    }
+  }
+}
