aeac211f004c3be2c697c4f2efdacffc24dc9ed1
Robin Carozzani <contact@robincarozzani.fr>
2019-09-22 12:42:49 +0200

    password generator template

---
+++ src/model/generatorOptions.ts
@@ -0,0 +1,9 @@
+export class GeneratorOptions {
+  public lowerCase: boolean;
+  public upperCase: boolean;
+  public numbers: boolean;
+  public special: boolean;
+  public excluded: string[];
+  public additional: string[];
+  public length: number;
+}
